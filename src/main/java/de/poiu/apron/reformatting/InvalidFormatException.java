19940f453b6b5b004c442e1f02206bc8707f100c
Marco Herrn <marco@mherrn.de>
2018-11-16 22:02:39 +0100

    Avoid warning on javadoc generation

--- src/main/java/de/poiu/apron/reformatting/InvalidFormatException.java
+++ src/main/java/de/poiu/apron/reformatting/InvalidFormatException.java
@@ -25,6 +25,9 @@ package de.poiu.apron.reformatting;
 public class InvalidFormatException extends RuntimeException {
 
   /**
+   * Constructs a new InvalidFormatException with {@code null} as its
+   * detail message.  The cause is not initialized, and may subsequently be
+   * initialized by a call to {@link #initCause}.
    * @see java.lang.RuntimeException#RuntimeException()
    */
   public InvalidFormatException() {
@@ -32,6 +35,12 @@ public class InvalidFormatException extends RuntimeException {
 
 
   /**
+   * Constructs a new InvalidFormatException with the specified detail message.
+   * The cause is not initialized, and may subsequently be initialized by a
+   * call to {@link #initCause}.
+   *
+   * @param   message   the detail message. The detail message is saved for
+   *          later retrieval by the {@link #getMessage()} method.
    * @see java.lang.RuntimeException#RuntimeException(java.lang.String)
    */
   public InvalidFormatException(String message) {
@@ -40,6 +49,17 @@ public class InvalidFormatException extends RuntimeException {
 
 
   /**
+   * Constructs a new InvalidFormatException with the specified detail message and
+   * cause.  <p>Note that the detail message associated with
+   * {@code cause} is <i>not</i> automatically incorporated in
+   * this runtime exception's detail message.
+   *
+   * @param  message the detail message (which is saved for later retrieval
+   *         by the {@link #getMessage()} method).
+   * @param  cause the cause (which is saved for later retrieval by the
+   *         {@link #getCause()} method).  (A {@code null} value is
+   *         permitted, and indicates that the cause is nonexistent or
+   *         unknown.)
    * @see java.lang.RuntimeException#RuntimeException(java.lang.String, java.lang.Throwable)
    */
   public InvalidFormatException(String message, Throwable cause) {
@@ -48,6 +68,16 @@ public class InvalidFormatException extends RuntimeException {
 
 
   /**
+   * Constructs a new InvalidFormatException with the specified cause and a
+   * detail message of {@code (cause==null ? null : cause.toString())}
+   * (which typically contains the class and detail message of
+   * {@code cause}).  This constructor is useful for runtime exceptions
+   * that are little more than wrappers for other throwables.
+   *
+   * @param  cause the cause (which is saved for later retrieval by the
+   *         {@link #getCause()} method).  (A {@code null} value is
+   *         permitted, and indicates that the cause is nonexistent or
+   *         unknown.)
    * @see java.lang.RuntimeException#RuntimeException(java.lang.Throwable)
    */
   public InvalidFormatException(Throwable cause) {
@@ -56,6 +86,17 @@ public class InvalidFormatException extends RuntimeException {
 
 
   /**
+   * Constructs a new InvalidFormatException with the specified detail
+   * message, cause, suppression enabled or disabled, and writable
+   * stack trace enabled or disabled.
+   *
+   * @param  message the detail message.
+   * @param cause the cause.  (A {@code null} value is permitted,
+   * and indicates that the cause is nonexistent or unknown.)
+   * @param enableSuppression whether or not suppression is enabled
+   *                          or disabled
+   * @param writableStackTrace whether or not the stack trace should
+   *                           be writable
    * @see java.lang.RuntimeException#RuntimeException(java.lang.String, java.lang.Throwable, boolean, boolean)
    */
   public InvalidFormatException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
